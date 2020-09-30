f167b40130fd216620dcceda36acb8932a54b2a7
FOOZBY <leha.sharonin2001@mail.ru>
2020-09-30 19:11:51 +0300

    Добавьте файлы проекта.

---
+++ 2ndlaba.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>                                 
+#include <cmath>            
+using namespace std;
+#define PI 3.14159265        
+
+int main()
+{
+    setlocale(0, "");
+    double x,y;
+    cout << "Введите x, y: " << endl;
+    cout << "x: "; cin >> x;
+    cout << "y: "; cin >> y;
+    cout << "g = " << 5*atan(x) - atan(y)/4 << " rad" << endl;
+    return 0;
+}
